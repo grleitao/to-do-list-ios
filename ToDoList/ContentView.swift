780712aee4335303468855288172dd6d58c36321
grleitaon <leitaozu@gmail.com>
2024-11-24 15:47:12 -0300

    initial commit

---
+++ ToDoList/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ToDoList
+//
+//  Created by Gustavo Rodrigues Leitao on 24/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Gustavo Rodrigues Leitao on 24/11/24.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
